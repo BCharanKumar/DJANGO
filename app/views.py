58d27963a130223d4f0ae3086a0bed359f3ddaf4
BCharanKumar <kumarbcharan77@gmail.com>
2024-10-15 20:41:49 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def example(request):
+    #Here I am taking HttpRequest and I am returning HttpResponse
+    return HttpResponse('i am from views')
